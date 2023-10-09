551f729c18746ab3701e2fb51f8ab94ce8d70b1e
Aldy Rizky Putra <aakendev@gmail.com>
2023-10-09 23:13:12 +0700

    React Challenge - Date Counter - Upload to GitHub

---
+++ README.md
@@ -0,0 +1,10 @@
+# React Challenge - Date Counter
+
+## by Aldy Rizky Putra
+
+### Challenge :
+
+- Use Date object to declare today date, past date, and futures date
+- Use useState to render and collect data from forms
+- Use Math Object to change negative numbers into absolute/positive
+- Use ternary condition to select views.
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date(Date.now());
+  date.setDate(date.getDate() + count);
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? 'Today is '
+            : count > 0
+            ? `${count} days from today is `
+            : count < 0
+            ? `${Math.abs(count)} days ago was `
+            : ''}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
