bbab19ba49ba02f08245e9b4376122a377ae8ae2
Aldy Rizky Putra <aakendev@gmail.com>
2023-10-11 05:59:37 +0000

    Update Adding Input Range and Input Number

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Counter />
     </div>
   );
@@ -14,30 +14,49 @@ function Counter() {
 
   const date = new Date(Date.now());
   date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
   return (
     <div>
       <div>
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>{step}</span>
       </div>
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <span>
+          <input
+            type="number"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
+        </span>
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
         <span>
           {count === 0
-            ? 'Today is '
+            ? "Today is "
             : count > 0
             ? `${count} days from today is `
             : count < 0
             ? `${Math.abs(count)} days ago was `
-            : ''}
+            : ""}
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      <div>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
